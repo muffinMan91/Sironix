19f054320de8bd7b7392cc6e775e83eb22578516
muffinMan91 <raymalik2099@gmail.com>
2023-12-13 14:58:46 -0500

    smth

--- server/app.js
+++ server/app.js
@@ -26,6 +26,17 @@ const { isLoggedIn } = require('./utils/middleware.js')
 const User = require('../models/User.js');
 const AppError = require('./routes/routeErrors/AppError');
 
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+//uncomment in production
+app.use(requireHTTPS);
+
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
@@ -71,16 +82,6 @@ passport.deserializeUser(async (data, done) => {
     }
 });
 
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
-//uncomment in production
-app.use(requireHTTPS);
 
 
 
