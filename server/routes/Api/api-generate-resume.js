abec4602ae26a2c4cff054727f18dba00d28ab48
muffinMan91 <raymalik2099@gmail.com>
2024-01-07 20:05:14 -0500

    trying to fix a fucin bug

--- server/routes/Api/api-generate-resume.js
+++ server/routes/Api/api-generate-resume.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createResumeFromData } = require('../../RouteHelpers/ResumeHelper');
+const { createResumeFromData } = require('../RouteHelpers/ResumeHelper.js');
 
 
 
