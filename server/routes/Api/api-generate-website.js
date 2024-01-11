22f593fe216d5e3640973260cf0d66de8294ab45
muffinMan91 <raymalik2099@gmail.com>
2024-01-10 19:31:58 -0500

    adding rest of website generation

--- server/routes/Api/api-generate-website.js
+++ server/routes/Api/api-generate-website.js
@@ -9,9 +9,15 @@ router.post('/createWebsiteAPI', async (req, res) => {
     try {
         //get the email from the body
         const email = req.body.email;
+        //get the title from the body
+        const title = req.body.title;
+        console.log("title", title);
         //get the subtitle from the body
         const subTitle = req.body.subtitle;
         console.log("subtitle", subTitle);
+        //get the schoolSummary from the body
+        const schoolSummary = req.body.schoolSummary;
+        console.log("schoolSummary", schoolSummary);
 
         res.status(200).json({ success: true, message: 'Website Link Generated' });
     } catch (error) {
